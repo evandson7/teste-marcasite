23fd6ecef9ae0781909e505b7138ebe5bdb70d4a
Evandson <evandson.sn@gmail.com>
2024-08-13 14:20:32 -0300

    criando db com sqlite

--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -1,9 +1,11 @@
 <x-layout title="Novo Aluno">
-    <form action="" method="post">
+    <form action="series/salvar" method="post">
+        @csrf
         <div class="mb-3">
             <label for="nome" class="form-label">Nome:</label>
             <input type="text" id="nome" name="nome" class="form-control">
         </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+
+        <button type="submit" class="btn btn-primary">Salvar</button>
     </form>
 </x-latout>
